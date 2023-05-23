8d4432f888e5b7749169732368d786aa857b0994
lucasjmr <130283171+lucasjmr@users.noreply.github.com>
2023-05-23 20:08:03 +0200

    Update input.h

--- WINDOWS/input.h
+++ WINDOWS/input.h
@@ -2,9 +2,5 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifdef __linux__
-    #include <errno.h>
-#endif
-
 void ask_string(char *string, int sizeofanswer, char *answer);
 void ask_int(char *string, int *answer);
