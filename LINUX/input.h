fdc62ac38ca5f302a4af8fcc71b5f149e83c086f
lucasjmr <130283171+lucasjmr@users.noreply.github.com>
2023-05-24 00:01:23 +0200

    Delete LINUX directory
    new version

--- LINUX/input.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-
-void ask_string(char *string, int sizeofanswer, char *answer);
-void ask_int(char *string, int *answer);
